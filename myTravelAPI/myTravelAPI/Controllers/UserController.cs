eda955930af7c864b9e0d2fcf7b11d21fc627ea3
andreamoncayo13 <andrea.moncayo2013@gmail.com>
2023-04-15 15:31:17 -0400

    initial commit

---
+++ myTravelAPI/myTravelAPI/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using myTravelAPI.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using myTravelAPI.Models;
+using Microsoft.Identity.Client;
+namespace myTravelAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+        public UserController(AppDbContext appDbContext)
+        {
+            _authContext = appDbContext;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            var user = await _authContext.Users.FirstOrDefaultAsync(x => x.UserName == userObj.UserName && x.Password == userObj.Password);
+            if (user == null)
+                return NotFound(new { Message = "User Not Found!" });
+
+            return Ok(new
+            {
+                Message = "Successful Login"
+            });
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+            await _authContext.Users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+            return Ok(new
+            {
+                Message = "User Registered!"
+            });
+        }
+    }
+}
